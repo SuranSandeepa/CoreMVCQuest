00f634359fbc348b4787a908fca5e6abd41fa6d0
SuranSandeepa <sandeepa.uththamawadu@gmail.com>
2024-12-22 02:10:31 +0530

    Add project files.

---
+++ webApplication/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webApplication.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return new ContentResult { Content = id.ToString() };
+        }
+    }
+}
